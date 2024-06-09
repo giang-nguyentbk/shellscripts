7f8be68711a73de157d84a6319ea918cb735e6d8
Giang Nguyen <giang.nguyentbk@gmail.com>
2024-06-09 20:53:57 +0700

    NOREF: Optimize cloning repos
    Instead of always cloning repos, we consider
    whether the latest commid id is different from local.
    If yes, we git pull --rebase, else dothing (even recompile).
    
    Team: Caliburn
    giangnguyentbk

--- env.sh
+++ env.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 SCRIPTPATH="$(readlink -f ${BASH_SOURCE[0]} | xargs dirname)"
-echo $SCRIPTPATH
+# echo $SCRIPTPATH
 
 echo "Exporting  SDKSYSROOT=$SCRIPTPATH"
 export SDKSYSROOT="$SCRIPTPATH"
+
+# Used for restore original LD_LIBRARY_PATH purpose
+echo "Exporting  ORIGINAL_LD_LIBRARY_PATH=$LD_LIBRARY_PATH (to be restored)"
+export ORIGINAL_LD_LIBRARY_PATH="$LD_LIBRARY_PATH"
 
 echo "Exporting  LD_LIBRARY_PATH=$SDKSYSROOT/usr/lib:\$LD_LIBRARY_PATH"
 export LD_LIBRARY_PATH="$SDKSYSROOT/usr/lib:$LD_LIBRARY_PATH"
