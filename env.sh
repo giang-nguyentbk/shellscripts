d7e78077694c88773a142b6119277e82cc4e741d
Giang Nguyen <giang.nguyentbk@gmail.com>
2024-06-09 19:46:51 +0700

    NOREF: Update way to get SDKSYSROOT
    Team: Caliburn
    giangnguyentbk

--- env.sh
+++ env.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-echo "Exporting  SDKSYSROOT=$(pwd)"
-export SDKSYSROOT="$(pwd)"
+
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+# echo $SCRIPTPATH
+
+echo "Exporting  SDKSYSROOT=$SCRIPTPATH"
+export SDKSYSROOT="$SCRIPTPATH"
 
 echo "Exporting  LD_LIBRARY_PATH=$SDKSYSROOT/usr/lib:\$LD_LIBRARY_PATH"
 export LD_LIBRARY_PATH="$SDKSYSROOT/usr/lib:$LD_LIBRARY_PATH"
